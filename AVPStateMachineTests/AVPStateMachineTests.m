ebec4461624189be8ae7f746d123c27abbe36ca6
Alexey Patosin <apatosin@yandex-team.ru>
2015-02-28 22:44:33 +0300

    OCMock appended to the project

--- AVPStateMachineTests/AVPStateMachineTests.m
+++ AVPStateMachineTests/AVPStateMachineTests.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 TestOrg. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 #import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
 #import "AVPStateMachine.h"
